3b175aa3e3b825eb12faa19e1f54a510ec2e27a0
tamadekk <186jnd@gmail.com>
2025-10-18 23:18:55 +0200

    chore: import css styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
+import "./index.css";
 
 const router = createRouter({ routeTree });
 
