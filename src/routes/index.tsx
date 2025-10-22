8b102e6df1744c6c6b25c381f6933b9988f25acf
tamadekk <186jnd@gmail.com>
2025-10-22 14:46:26 +0200

    feat: add all sections to index page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
+import HeroSection from "@/components/hero-section";
+import CategoriesSection from "@/components/categories-section";
+import HowItWorksSection from "@/components/how-it-works-section";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
 
 function HomeComponent() {
-  return <div className="p-2"></div>;
+  return (
+    <>
+      <HeroSection />
+      <CategoriesSection />
+      <HowItWorksSection />
+    </>
+  );
 }
