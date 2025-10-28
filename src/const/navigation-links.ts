b0dd9bb3b55835f56217f58c5594a14a96be4a41
tamadekk <186jnd@gmail.com>
2025-10-28 08:27:23 +0100

    chore: change route name

--- src/const/navigation-links.ts
+++ src/const/navigation-links.ts
@@ -16,7 +16,7 @@ export const navigationItems: navigationItem[] = [
   { title: "Home", href: "/", icon: House },
   { title: "Services", href: "/services", icon: List },
   { title: "Post a service", href: "/", icon: Plus },
-  { title: "My profile", href: "/profile", icon: CircleUser },
+  { title: "My dashboard", href: "/dashboard", icon: CircleUser },
   { title: "About", href: "/about", icon: Info },
 ];
 
