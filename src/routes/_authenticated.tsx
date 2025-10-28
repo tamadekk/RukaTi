7b4e0672bd08f5440c9f3e7f1efc1eb9e41e9100
tamadekk <186jnd@gmail.com>
2025-10-28 08:55:02 +0100

    feat: add user auth check for protected routes

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -1,9 +1,10 @@
+import { useUserSession } from "@/store/userSessionsStore";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated")({
   beforeLoad: ({ location }) => {
-    /* TODO: check if user is authenticated */
-    if (false) {
+    const user = useUserSession.getState().user;
+    if (!user?.id) {
       throw redirect({
         to: "/login",
         search: {
