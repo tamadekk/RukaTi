b01c20e68c6395cdc300c473145f6ff2ec24a620
tamadekk <186jnd@gmail.com>
2025-12-13 14:42:35 +0100

    chore: remove sidebar until needed

--- src/components/profile-dashboard.tsx
+++ src/components/profile-dashboard.tsx
@@ -1,25 +1,19 @@
-import Sidebar from "@/components/dashboard/profile/sidebar";
 import ServicePerformance from "@/components/dashboard/profile/service-performance";
 import QuickActions from "@/components/dashboard/profile/quick-actions";
 import MainSection from "./dashboard/main-section-dashboard";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { useInitializeUser } from "@/hooks/init-userProfile";
 
 export default function ProfileDashboard() {
   useInitializeUser();
   return (
     <div className="flex flex-col min-h-screen bg-[#f7f8fa] md:flex-row">
-      <SidebarProvider>
-        <Sidebar />
-        <SidebarTrigger />
-        <main className="flex-1 p-2 sm:p-4 md:p-8 flex flex-col lg:flex-row gap-4 md:gap-6 mt-20 md:mt-0">
-          <aside className="w-full flex flex-col gap-4 md:gap-6">
-            <MainSection />
-            <ServicePerformance />
-            <QuickActions />
-          </aside>
-        </main>
-      </SidebarProvider>
+      <main className="flex-1 p-2 sm:p-4 md:p-8 flex flex-col lg:flex-row gap-4 md:gap-6 mt-20 md:mt-0">
+        <aside className="w-full flex flex-col gap-4 md:gap-6">
+          <MainSection />
+          <ServicePerformance />
+          <QuickActions />
+        </aside>
+      </main>
     </div>
   );
 }
