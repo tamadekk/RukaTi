e07db73775792132d8fa550da30d2d76b1506344
tamadekk <186jnd@gmail.com>
2026-03-09 23:50:48 +0100

    feat: introduces jest

---
+++ jest.setup.ts
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+
+// Mock localStorage to fix zustand persist warning in testing environments like Bun
+if (typeof globalThis.localStorage === "undefined") {
+  globalThis.localStorage = {
+    getItem: () => null,
+    setItem: () => {},
+    removeItem: () => {},
+    clear: () => {},
+    length: 0,
+    key: () => null,
+  } as any;
+}
