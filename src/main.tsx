918f3189eeeeb9822ce3537781cb7ec61cd38d99
tamadekk <186jnd@gmail.com>
2026-04-22 21:23:09 +0200

    feat: enable scrollRestoration when navigating

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "@/routeTree.gen";
 import "./index.css";
 
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, scrollRestoration: true });
 
 declare module "@tanstack/react-router" {
   interface Register {
