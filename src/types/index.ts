0afd60a1753afea6e20b4337ce90ab0b359fd57b
tamadekk <186jnd@gmail.com>
2026-04-25 12:01:38 +0200

    feat: add typing for GeoApify

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,3 +11,12 @@ export type FooterLink = {
   title: string;
   href: string;
 };
+
+export type GeoapifySuggestion = {
+  formatted: string;
+  address_line1?: string;
+  address_line2?: string;
+  city?: string;
+  name?: string;
+  country?: string;
+};
